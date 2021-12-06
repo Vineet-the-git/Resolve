0777c42fb6d02a540f19bd48c8f66c228dd25f9c
Vineet Kumar <vineet.kmr1708@gmail.com>
2021-12-06 15:12:40 +0530

    sidebar created

--- resolve_new/src/Components/Sidebar/Sidebar.js
+++ resolve_new/src/Components/Sidebar/Sidebar.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import "./_sidebar.css";
-import ChatIcon from '@mui/icons-material/Chat';
-import DonutLargeIcon from '@mui/icons-material/DonutLarge';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Avatar, IconButton } from '@mui/material';
 import TasksOption from './Sidebar_items/Tasks';
 import StatisticsOption from './Sidebar_items/Statistics';
@@ -10,39 +7,54 @@ import LogbookOption from './Sidebar_items/Logbook_tab';
 import CalendarOption from './Sidebar_items/Calendar_tab';
 import ContactUsOption from './Sidebar_items/ContactUs_tab';
 import HelpOption from './Sidebar_items/Help_tab';
+import { useHistory } from 'react-router';
 
 
 function Sidebar() {
+
+    const history = useHistory();
+    const [value, setValue] = React.useState(history.location.pathname);
+
+    const handleChange = (newValue) => {
+        setValue(newValue);
+        history.push(newValue);
+        console.log(newValue);
+    };
     return (
         <div className="sidebar">
             <div className="sidebar_header">
                 <Avatar src="https://avatars.githubusercontent.com/u/80666035?s=400&u=8beb83d2960c259d702f9f4296b900d1f135b101&v=4" />
-                <div className="sidebar_headerRight">
-                    <IconButton>
-                        <DonutLargeIcon />
-                    </IconButton>
-                    <IconButton>
-                        <ChatIcon />
-                    </IconButton>
-                    <IconButton>
-                        <MoreVertIcon />
-                    </IconButton>
 
+            </div>
+
+            <div className="sidebar_items" >
+                <div onClick={(event) => handleChange("/tasks")}>
+                    <TasksOption />
+                </div>
+
+                <div onClick={(event) => handleChange("/statistics")}>
+                    <StatisticsOption />
+                </div>
+
+                <div onClick={(event) => handleChange("/logbook")}>
+                    <LogbookOption />
+                </div>
+
+                <div onClick={(event) => handleChange("/calendar")}>
+                    <CalendarOption />
+                </div>
+
+                <div onClick={(event) => handleChange("/contactus")}>
+                    <ContactUsOption />
+                </div>
+
+                <div onClick={(event) => handleChange("/help")}>
+                    <HelpOption />
                 </div>
 
             </div>
 
-            <div className="sidebar_chats">
-                <TasksOption />
-                <StatisticsOption />
-                <LogbookOption/>
-                <CalendarOption/>
-                <ContactUsOption/>
-                <HelpOption/>
-
-            </div>
-
-        </div>
+        </div >
     )
 }
 
