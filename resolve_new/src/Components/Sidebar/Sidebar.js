180a0cb1fb8519bbc8f907602ed30e8a69b1d67c
Vineet Kumar <vineet.kmr1708@gmail.com>
2021-12-08 17:27:00 +0530

    styling done

--- resolve_new/src/Components/Sidebar/Sidebar.js
+++ resolve_new/src/Components/Sidebar/Sidebar.js
@@ -8,6 +8,8 @@ import CalendarOption from './Sidebar_items/Calendar_tab';
 import ContactUsOption from './Sidebar_items/ContactUs_tab';
 import HelpOption from './Sidebar_items/Help_tab';
 import { useHistory } from 'react-router';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
 
 
 function Sidebar() {
@@ -23,32 +25,43 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar_header">
-                <Avatar src="https://avatars.githubusercontent.com/u/80666035?s=400&u=8beb83d2960c259d702f9f4296b900d1f135b101&v=4" />
+                <div className = "sidebar_headerLeft">
+                    <Avatar src="https://avatars.githubusercontent.com/u/80666035?s=400&u=8beb83d2960c259d702f9f4296b900d1f135b101&v=4" />
+                </div>
+
+                <div className="sidebar_headerRight">
+                    <IconButton id="icon">
+                        <StarOutlineIcon />
+                    </IconButton>
+                    <IconButton id="icon">
+                        <NotificationsActiveOutlinedIcon />
+                    </IconButton>
+                </div>
 
             </div>
 
             <div className="sidebar_items" >
-                <div onClick={(event) => handleChange("/tasks")}>
+                <div className="item" onClick={(event) => handleChange("/tasks")}>
                     <TasksOption />
                 </div>
 
-                <div onClick={(event) => handleChange("/statistics")}>
+                <div className="item" onClick={(event) => handleChange("/statistics")}>
                     <StatisticsOption />
                 </div>
 
-                <div onClick={(event) => handleChange("/logbook")}>
+                <div className="item" onClick={(event) => handleChange("/logbook")}>
                     <LogbookOption />
                 </div>
 
-                <div onClick={(event) => handleChange("/calendar")}>
+                <div className="item" onClick={(event) => handleChange("/calendar")}>
                     <CalendarOption />
                 </div>
 
-                <div onClick={(event) => handleChange("/contactus")}>
+                <div className="item" onClick={(event) => handleChange("/contactus")}>
                     <ContactUsOption />
                 </div>
 
-                <div onClick={(event) => handleChange("/help")}>
+                <div className="item" onClick={(event) => handleChange("/help")}>
                     <HelpOption />
                 </div>
 
