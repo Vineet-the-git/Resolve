0edc2f0704b9616110f148484459a76e1848c858
Vineet Kumar <vineet.kmr1708@gmail.com>
2021-12-13 18:48:36 +0530

    upgrade

--- resolve_new/src/Pages/Statistics/Statistics.js
+++ resolve_new/src/Pages/Statistics/Statistics.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "C:/Users/vinee/Desktop/Resolve/resolve_new/src/Pages/Statistics/_statistics.css";
 
 function Statistics() {
     return (
-        <div>
-            <h1>These are your stats </h1>
+        <div className = "main_template">
+            <div className="grid-item">
+                Overview
+            </div>
+            <div className="grid-item">
+                Achievement Score
+            </div>
+            <div className="grid-item">
+                Completetion Rate
+            </div>
+            <div className="grid-item">
+                Completed Task Percentage
+            </div>
+            
         </div>
     )
 }
