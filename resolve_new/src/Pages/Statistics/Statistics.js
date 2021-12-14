5b1b42e92458337ecc45cf105bc4e51be8562380
Vineet Kumar <vineet.kmr1708@gmail.com>
2021-12-14 12:19:46 +0530

    upgrade

--- resolve_new/src/Pages/Statistics/Statistics.js
+++ resolve_new/src/Pages/Statistics/Statistics.js
@@ -3,9 +3,17 @@ import "C:/Users/vinee/Desktop/Resolve/resolve_new/src/Pages/Statistics/_statist
 
 function Statistics() {
     return (
-        <div className = "main_template">
+        <div className="main_template">
             <div className="grid-item">
-                Overview
+                <h3>Overview</h3>
+                <div className='overview-entries'>
+                    <h2 className='td td1'>15</h2>
+                    <h2 className='td'>15</h2>
+                    <h2 className='td'>8:25</h2>
+                    <h2 className='td td2'>1656</h2>
+                    <h2 className='td'>2512</h2>
+                    <h2 className='td'>1024:32</h2>
+                </div>
             </div>
             <div className="grid-item">
                 Achievement Score
@@ -16,7 +24,7 @@ function Statistics() {
             <div className="grid-item">
                 Completed Task Percentage
             </div>
-            
+
         </div>
     )
 }
